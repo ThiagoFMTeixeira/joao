2d175d4af7604d1e0993c5a7f2c6bb2683f5230e
Joao Abreu <Jmd25454@gmail.com>
2021-06-07 19:45:36 -0300

    Criei uma classe automóvel visando o lucro das comunidades brasileiras.
    Signed-off-by: Joao Abreu <Jmd25454@gmail.com>

---
+++ src/main/java/com/example/demo/Automovel.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Automovel {
+
+    private int quantidadeCores;
+    private int espaco;
+    private double velocidadeMed;
+    private double preco;
+    private String nome;
+    private String classe;
+
+    void informarCarro() {
+        System.out.println("Esse é o carro " + nome);
+    }
+}
